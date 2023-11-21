ed703fa88998cf436cfdb8255cfefb8bf971b165
Lukas Gessl <lukas.gessl@stud.uni-regensburg.de>
2023-11-21 16:18:09 +0000

    read expression data into matrix, pheno_tbl in tidy format

--- R/read.R
+++ R/read.R
@@ -1,6 +1,6 @@
-#' @title Read expression and pheno data from csv files into tibbles
-#' @description Read expression and pheno csv files into tibbles and move
-#' patient and gene identifiers to row names.
+#' @title Read expression and pheno data from csv files into consistent format
+#' @description Read expression data into a matrix and pheno data into a tibble.
+#' Both will hold patients as rows.
 #' @param directory string. The directory where both expression and pheno csv
 #' files lie.
 #' @param expr_fname string. The name of the expression .csv file inside `directory`.
@@ -9,37 +9,50 @@
 #' See details for the expected format.
 #' @param patient_id_col string. The name of the column in the pheno data file
 #' that holds the patient identifiers.
-#' @param gene_col string. The name of the column in the expression data file
+#' @param gene_id_col string. The name of the column in the expression data file
 #' that holds the gene identifiers.
-#' @details The pheno .csv files holds the samples as rows (with the unique sample names
-#'  in the very first column), the variables as columns. We need at least the columns
+#' @return A list with a numeric matrix, named `expr`, and a tibble named `pheno`. 
+#' `expr` holds the expression data, with patient ids as row names and gene ids as
+#' column names. I.e., we transpose the expression data. `pheno` holds the pheno data, 
+#' with the patient ids in the first column `patient_id_col`.
+#' @details The pheno csv file holds the samples as rows (with the unique sample names
+#'  in the column `patient_id`), the variables as columns. We need at least the columns
 #'  * progression (integer, 0 for censored, 1 for uncensored),
 #'  * pfs_yrs (integer, the time in years to progression or censoring),
 #'  * `ìnclude_from_continuous_pheno`,
 #'  * `include_from_discrete_pheno` and
 #'  * `patient_id_col`.
-#' The expr .csv file holds the genes as rows (with the unique genes in a row called 
-#' `gene_col`), the samples as columns. The sample names in pheno and expression files
+#' The expr csv file holds the genes as rows (with the gene ids in a row called 
+#' `gene_id_col`), the samples as columns. The sample names in pheno and expression files
 #' must be identical and in the same order.
 read <- function(
     directory,
     expr_fname,
     pheno_fname,
     patient_id_col,
-    gene_col
+    gene_id_col
 ){
+    # read
     fnames <- c(expr_fname, pheno_fname)
     res <- list("expr" = NULL, "pheno" = NULL)
     for (i in 1:length(fnames)){
-        full_path <- file.path(data_dir, fnames[i])
-        res[[i]] <- readr::read_csv(full_path)
+        full_path <- file.path(directory, fnames[i])
+        res[[i]] <- readr::read_csv(full_path, show_col_types = FALSE)
     }
-    
-    # actual data is actual data frame
-    res[["expr"]] <- expr_df %>% 
-        tibble::column_to_rownames(var = "Gene")
-    res[["pheno"]] <- pheno_df %>%
-        tibble::column_to_rownames(var = "patient_id")
+    expr_tbl <- res[["expr"]]
+    pheno_tbl <- res[["pheno"]]
+
+    # expression to matrix
+    gene_names <- res[["expr"]][[gene_id_col]]
+    expr_mat <- expr_tbl |>
+        dplyr::select(!all_of(gene_id_col)) |>
+        as.matrix() |> 
+        t()
+    colnames(expr_mat) <- gene_names
+
+    # pheno: move patient ids into first column
+    pheno_tbl <- pheno_tbl |>
+        dplyr::relocate(all_of(patient_id_col))
 
     return(res)
 }
