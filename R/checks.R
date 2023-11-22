5f29cc785ad2f3ae2239b49dc9d2d2e10a306ac8
Lukas Gessl <lukas.gessl@stud.uni-regensburg.de>
2023-11-22 16:43:04 +0000

    introduce 2 checks

---
+++ R/checks.R
@@ -0,0 +1,52 @@
+#' @title Check if a tibble has certain columns
+#' @param tbl tibble. The tibble to check.
+#' @param tbl_name string. How to refer to the tibble in an error message.
+#' @param col_names vector of strings. The names of the columns to check for.
+#' @return NULL. Throws an error if one of the columns is not in the tibble.
+check_tbl_columns_exist <- function(
+    tbl,
+    tbl_name,
+    col_names
+){
+    for(cname in col_names){
+        if(is.null(tbl[[cname]])){
+            stop("Column ", cname, " does not exist in ", tbl_name, ".")
+        }
+    }
+}
+
+check_consistent_patient_ids <- function(
+    stage,
+    expr,
+    pheno,
+    patient_id_col = NULL,
+    gene_id_col = NULL
+){
+    # extract patient ids from expression and pheno data
+    patient_ids_expr <- NULL
+    patient_ids_pheno <- NULL
+    # case 1: during preprocessing
+    if(stage == "preprocessing"){
+        patient_ids_expr <- colnames(expr)[-1]
+        patient_ids_pheno <- pheno[[patient_id_col]]
+    }
+    # case 2: right before generate_predictor
+    if(stage == "before_generate_predictor"){
+        patient_ids_expr <- rownames(expr)
+        patient_ids_pheno <- pheno[[patient_id_col]]
+    }
+    # case 3: after generate_predictor and generate_response
+    if(stage == "after_generate_xy"){
+        patient_ids_expr <- rownames(expr)
+        patient_ids_pheno <- rownames(pheno)
+    }
+
+    if(length(c(patient_ids_expr, patient_ids_pheno)) < 2){
+        stop("stage must be on of 'preprocessing', \
+            'before_generate_predictor', 'after_generate_xy'.")
+    }
+
+    if(!identical(patient_ids_expr, patient_ids_pheno)){
+        stop("Patient ids in expression and pheno data are not identical.")
+    }
+}
