a5b2fa4c242bae8699646fddea8c5f04debb53d8
Lukas Gessl <lukas.gessl@stud.uni-regensburg.de>
2024-01-18 16:07:58 +0000

    take into account deleted Schmitz mock data

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -1,11 +1,22 @@
 test_that("read function works correctly", {
 
+  n_samples <- 5
+  n_genes <- 2
+
+  dir <- withr::local_tempdir()
+  generate_mock_data(
+    n_samples = n_samples,
+    n_genes = n_genes,
+    n_na_in_pheno = 3,
+    to_csv = dir,
+    split_index = 1:3
+  )
   data_spec <- DataSpec(
-    name = "Schmitz et al. (2018)",
-    directory = "data/schmitz",
+    name = "mock",
+    directory = dir,
     train_prop = .7,
-    expr_fname = "expr1.csv",
-    pheno_fname = "pheno1.csv",
+    expr_fname = "expr.csv",
+    pheno_fname = "pheno.csv",
     patient_id_col = "patient_id",
     gene_id_col = "gene_id"
   )
@@ -15,17 +26,15 @@ test_that("read function works correctly", {
   expr_mat <- result[["expr_mat"]]
   pheno_tbl <- result[["pheno_tbl"]]
 
-  expect_type(expr_mat, "double")
-  expect_true(inherits(expr_mat, "matrix"))
+  expect_true(is.numeric(expr_mat))
+  expect_true(is.matrix(expr_mat))
   expect_s3_class(pheno_tbl, "tbl_df")
   expect_equal(colnames(pheno_tbl)[1], data_spec$patient_id_col)
 
   # errors
   # directory does not exist
-  data_spec$directory <- "nonexistent.csv"
-  expect_error(read(data_spec))
   data_spec$patient_id_col <- "missing_patient_id_col"
-  expect_error(read(data_spec))
+  expect_error(read(data_spec), "no column named")
   data_spec$gene_id_col <- "missing_gene_id"
-  expect_error(read(data_spec))
+  expect_error(read(data_spec), "no column named")
 })
