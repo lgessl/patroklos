82c3a611461295a1b9b0f47637f486a263bf8ae1
Lukas Gessl <lukas.gessl@icloud.com>
2024-05-03 14:37:15 +0000

    first draft: assess models with truly binary output (a new feature of the AssScalar R6 class)

--- R/ass_scalar_helpers.R
+++ R/ass_scalar_helpers.R
@@ -25,18 +25,24 @@ ass_scalar_assess <- function(self, private, data, model, quiet){
         lambda = self$lambda,
         pivot_time_cutoff = self$pivot_time_cutoff
     )
-    core <- function(i){
+    res_mat <- matrix(.0, nrow = length(model$split_index), 
+        ncol = length(self$metric), byrow = TRUE)
+    for (i in seq_along(model$split_index)) {
         predicted <- prep[["predicted"]][[i]]
         actual <- prep[["actual"]][[i]]
         pa <- intersect_by_names(predicted, actual, rm_na = TRUE)
-        do.call(self$metric, list(
-            "predicted" =  pa[[1]],
-            "actual" = pa[[2]]
-            )
-        )
+        for (j in seq_along(self$metric)) {
+            metric <- paste0("get_", self$metric[j])
+            res_mat[i, j] <- do.call(metric, list(
+                "predicted" =  pa[[1]],
+                "actual" = pa[[2]],
+                "data" = data
+            ))
+        }
     }
-    metric <- vapply(seq_along(model$split_index), core, numeric(1))
-    return(metric)
+    colnames(res_mat) <- self$metric
+    rownames(res_mat) <- self$split_index
+    res_mat
 }
 
 ass_scalar_assess_center <- function(self, private, data, model_list,
@@ -54,27 +60,41 @@ ass_scalar_assess_center <- function(self, private, data, model_list,
         res_tbl <- tibble::tibble(
             "model" = character(m),
             "cohort" = character(m),
-            "cutoff" = numeric(m),
-            "mean" = numeric(m),
-            "sd" = numeric(m),
-            "min" = numeric(m),
-            "max" = numeric(m)
+            "cutoff" = numeric(m)
         )
+        if (length(self$metric == 1)) {
+            ncol_addon <- 4
+            colnames_addon <- c("mean", "sd", "min", "max")
+        } else {
+            ncol_addon <- length(self$metric)
+            colnames_addon <- self$metric
+        }
+        addon_tbl <- tibble::as_tibble(matrix(.0, nrow = m, ncol = ncol_addon))
+        names(addon_tbl) <- colnames_addon
+        res_tbl <- dplyr::bind_cols(res_tbl, addon_tbl)
         for(j in 1:m){
             time_cutoff <- model$time_cutoffs[j]
             model_cutoff <- model$at_time_cutoff(time_cutoff)
             res_tbl[j, 1] <- model$name
             res_tbl[j, 2] <- data$cohort
             res_tbl[j, 3] <- time_cutoff
-            metric <- self$assess(
+            metric_mat <- self$assess(
                 data,
                 model = model_cutoff,
                 quiet = quiet
             )
-            res_tbl[j, 4] <- round(mean(metric), digits = digits) 
-            res_tbl[j, 5] <- round(stats::sd(metric), digits = digits)
-            res_tbl[j, 6] <- round(min(metric), digits = digits)
-            res_tbl[j, 7] <- round(max(metric), digits = digits)
+            if (length(self$metric) == 1) {
+                metric <- metric_mat[, 1]
+                res_tbl[j, 4] <- round(mean(metric), digits = digits) 
+                res_tbl[j, 5] <- round(stats::sd(metric), digits = digits)
+                res_tbl[j, 6] <- round(min(metric), digits = digits)
+                res_tbl[j, 7] <- round(max(metric), digits = digits)
+            } else {
+                for(k in seq_along(self$metric)){
+                    metric <- metric_mat[, k]
+                    res_tbl[j, 3 + k] <- round(mean(metric), digits = digits)
+                }
+            }
         }
         res_tbl
     }
@@ -105,9 +125,41 @@ ass_scalar_assess_center <- function(self, private, data, model_list,
 
 get_auc <- function(
     predicted,
-    actual
+    actual,
+    ...
 ){
     pred_obj <- ROCR::prediction(predictions = predicted, labels = actual)
     res <- ROCR::performance(pred_obj, measure = "auc")
-    return(res@y.values[[1]])
+    res@y.values[[1]]
+}
+
+get_accuracy <- function(
+    predicted,
+    actual,
+    ...
+){
+    mean(predicted == actual)
+}
+
+get_precision <- function(
+    predicted,
+    actual,
+    ...
+){
+   mean(actual[predicted == 1]) 
+}
+
+get_logrank <- function(
+    predicted,
+    actual,
+    data,
+    ...
+){
+    pheno_tbl <- data$pheno_tbl
+    pheno_tbl <- pheno_tbl[pheno_tbl[[data$patient_id_col]] %in% names(predicted), ]
+    time <- pheno_tbl[[data$time_to_event_col]]
+    event <- pheno_tbl[[data$event_col]]
+    survival::survdiff(
+        survival::Surv(time = time, event = event) ~ predicted
+    )[["pvalue"]]
 }
--- tests/testthat/test-ass_scalar.R
+++ tests/testthat/test-ass_scalar.R
@@ -24,6 +24,7 @@ test_that("AssScalar$assess() works", {
     n_fold <- 1
     lambda <- 1
     split_index <- 1:2
+    metric <- c("auc", "accuracy", "precision", "logrank")
     
     model_dir <- withr::local_tempdir()
     data <- generate_mock_data(
@@ -33,26 +34,27 @@ test_that("AssScalar$assess() works", {
         split_index = split_index
     )
     model <- Model$new(
-        name = "logistic",
-        directory = model_dir,
-        fitter = zeroSum::zeroSum,
-        split_index = split_index,
-        time_cutoffs = 2.,
-        hyperparams = list(family = "binomial", alpha = 1, nFold = n_fold, 
-            lambda = lambda, zeroSum = FALSE),
-        response_type = "binary"
+      name = "rf",
+      directory = file.path(model_dir, "rf"),
+      fitter = ranger::ranger,
+      hyperparams = list(mtry = 2, num.trees = 100, min.node.size = 3),
+      split_index = 1:2,
+      time_cutoffs = 2,
+      response_type = "binary",
+      fit_file = "model.rds"
     )
     ass_scalar <- AssScalar$new(
-        metric = "get_auc",
-        pivot_time_cutoff = 2,
-        benchmark = "ipi",
-        round_digits = 4
+      metric = metric,
+      pivot_time_cutoff = 2,
+      benchmark = "ipi",
+      round_digits = 4
     )
     model$fit(data, quiet = TRUE)
 
-    auc <- ass_scalar$assess(data, model, quiet = TRUE)
-    expect_true(is.numeric(auc))
-    expect_equal(length(auc), length(split_index))
+    res <- ass_scalar$assess(data, model, quiet = TRUE)
+    expect_true(is.numeric(res))
+    expect_true(is.matrix(res))
+    expect_equal(dim(res), c(length(split_index, length(metric))))
 })
 
 test_that("AssScalar$assess_center() works", {
@@ -64,6 +66,8 @@ test_that("AssScalar$assess_center() works", {
   n_na_in_pheno <- 5
   n_fold <- 1
   lambda <- 1
+  multiple_metric <- c("accuracy", "precision")
+  single_metric <- "accuracy"
 
   dir <- withr::local_tempdir()
   data <- generate_mock_data(
@@ -72,46 +76,57 @@ test_that("AssScalar$assess_center() works", {
     n_na_in_pheno = n_na_in_pheno,
     to_csv = file.path(dir, "data")
   )
-  model_1 <- Model$new(
-    name = "cox-zerosum",
-    directory = file.path(dir, "models/cox"),
-    fitter = zeroSum::zeroSum,
-    split_index = 1:2,
-    time_cutoffs = 2.,
-    hyperparams = list(family = "cox", alpha = 1, nFold = n_fold, 
-      lambda = lambda, zeroSum = FALSE),
-    response_type = "survival_censored",
-    include_from_continuous_pheno = "continuous_var",
-    fit_file = "model1.rds"
-  )
-  model_2 <- Model$new(
+  model1 <- Model$new(
     name = "binomial-zerosum",
     directory = file.path(dir, "models/logistic"),
     fitter = zeroSum::zeroSum,
     hyperparams = list(family = "binomial", alpha = 1, nFold = n_fold, 
       lambda = lambda, zeroSum = FALSE),
-    split_index = 1:2,
+    split_index = 1,
     time_cutoffs = c(1.5, 2),
     response_type = "binary",
     include_from_discrete_pheno = "discrete_var",
     fit_file = "model2.rds"
   )
-  model_list <- list(model_1, model_2)
+  model2 <- Model$new(
+    name = "rf",
+    directory = file.path(dir, "models/rf"),
+    fitter = ptk_ranger,
+    hyperparams = list(mtry = 2, num.trees = 100, min.node.size = 3),
+    split_index = 1:2,
+    time_cutoffs = c(1.5, 2),
+    response_type = "binary",
+    fit_file = "model3.rds",
+    include_from_discrete_pheno = "discrete_var"
+  )
+  model1_list <- list(model1, model1)
+  model2_list <- list(model2, model2)
+  model_list <- list(model1, model2)
   training_camp(model_list, data, quiet = TRUE) 
   ass_scalar <- AssScalar$new(
-    metric = "get_auc",
+    metric = "auc", # just for the moment
     pivot_time_cutoff = 2,
-    file = file.path(dir, "models/auc.csv")
+    file = file.path(dir, "models/eval.csv")
   )
   dir.create(dir, "results")
+  
+  # Case 1: one metric
+  data$cohort <- "test"
+  ass_scalar$metric <- single_metric 
+  eval_tbl <- ass_scalar$assess_center(data, model1_list, quiet = TRUE)
+  expect_equal(nrow(eval_tbl), length(data$cohort) * (2+2))  
+  expect_equal(colnames(eval_tbl), c("model", "cohort", "cutoff", "mean", 
+    "sd", "min", "max"))
 
+  # Case 2: multiple metrics
   data$cohort <- c("train", "test")
-  auc_tbl <- ass_scalar$assess_center(data, model_list, quiet = TRUE)
-
+  ass_scalar$metric <- multiple_metric
+  eval_tbl <- ass_scalar$assess_center(data, model2_list, quiet = TRUE)
   expect_true(
-    file.exists(file.path(dir, "models/auc.csv")) &&
-    file.exists(file.path(dir, "results/auc.csv"))
+    file.exists(file.path(dir, "models/eval.csv")) &&
+    file.exists(file.path(dir, "results/eval.csv"))
   )
-  expect_equal(colnames(auc_tbl), c("model", "cohort", "cutoff", "mean", "sd", "min", "max"))
-  expect_equal(nrow(auc_tbl), 2 * (1+2))  
+  expect_equal(nrow(eval_tbl), length(data$cohort) * (2+2))  
+  expect_equal(colnames(eval_tbl), c("model", "cohort", "cutoff", metric))
+
 })
