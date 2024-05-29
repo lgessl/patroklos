2b2de6ed6059c401359e42f7f61c24313eb48708
Lukas Gessl <lukas.gessl@icloud.com>
2024-05-29 15:14:16 +0000

    tibble_to_binary(): test that NAs result in NAs

--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -1,19 +1,23 @@
 test_that("tibble_to_binary works correctly", {
   # Create a tibble for testing
   test_tibble <- tibble::tibble(
-    column1 = factor(c("a", "b", "c")),
-    column2 = factor(c("d", "e", "f"))
+    column1 = factor(c("a", "b", "b", "c")),
+    column2 = factor(c("d", "e", "f", "d"))
   )
   result <- tibble_to_binary(test_tibble)
 
   # Check the result
   expect_equal(
     dim(result), 
-    c(3, 4)
+    c(4, 4)
     ) # 3 rows, 4 columns (2 original columns, each with 3-1 levels)
   expect_equal(colnames(result), c("column1_b", "column1_c", "column2_e", "column2_f"))
   expect_true(all(result %in% c(0., 1.)))
   expect_error(tibble_to_binary(test_tibble[["column1"]]))
+
+  test_tibble[1, 2] <- NA
+  result <- tibble_to_binary(test_tibble)
+  expect_true(all(is.na(result[1, 3:4])))
 })
 
 test_that("intersect_by_names() works", {
